7303dd6d739ff50034d370dc4a923033da90c48e
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-01-20 13:50:27 +0100

    Codice aggiornato. Aggiunto ultimo accesso

--- js/script.js
+++ js/script.js
@@ -99,22 +99,31 @@ new Vue({
             this.id=index;
             // console.log(this.contacts[this.id].messages)
         },
-        // isInList:function(item){
-        //     for (let index = 0; index < this.newListContacts.length; index++) {
-        //         if (item === this.newListContacts[index]) {
-        //             return true;
-        //         }
-        //         return false;
+
+        // funzione che mi permette di sapere
+        // la data e l'orario attuale
+        dateNow() {
+            return dayjs().format('DD/MM/YYYY H:m:s')
+        },
+        /*
+        isInList:function(item){
+            for (let index = 0; index < this.newListContacts.length; index++) {
+                if (item === this.newListContacts[index]) {
+                    return true;
+                }
+                return false;
                 
-        //     };
+            };
             
-        // },
+        },
+        */
 
         // funzione che mi permette di inserire un oggetto 
         // nella lista di oggetti messages
+        // che poi sarà stampato nel documento HTML
         sentMessageTo(contact){
             contact.messages.push({
-                date: '10/01/2020 16:15:22',
+                date: this.dateNow(),
                 text: this.temporaryMessage,
                 status: 'sent'
             });
@@ -122,7 +131,7 @@ new Vue({
 
             setTimeout(() => {
                 this.contacts[this.id].messages.push({
-                    date: '10/01/2020 16:15:23',
+                    date: this.dateNow(),
                     text: "ok",
                     status: "received",
                 });
@@ -130,42 +139,59 @@ new Vue({
 
         },
 
+        // funzione che mi permette di 'filtrare' i contanti
         filtered(contact){
             return contact.name.toUpperCase().includes(this.searchContact.toUpperCase())
+        },
+
+        // funzione che mi permette ddi controllare
+        // lo sattus di un messaggio
+        // e in base al risultato di aggiungere una classe al documento HTML
+        checkStatus(message){
+            return message.status === 'sent' ? 'my-message' : 'other-message'
+        },
+
+        // funzione che mi fornisce la data
+        // dell'ultimo messaggio
+        // che successivamente sarà usata per l'ultimo accesso
+        lastAccess(){
+            const lunghezzaArray = this.contacts[this.id].messages
+            console.log(lunghezzaArray.length-1)
+            return this.contacts[this.id].messages[lunghezzaArray.length-1].date
         }
 
+        /*
+        funzione per filtrare i contatti
+        listaElementiFiltrati() {
+            const newListContacts = this.contacts.filter((contact)=>{
 
-        // funzione per filtrare i contatti
-        // listaElementiFiltrati() {
-        //     const newListContacts = this.contacts.filter((contact)=>{
-
-        //         return contact.name.toUpperCase().startsWith(this.searchContact.toUpperCase()) || this.searchContact ===''
-        //     });
-        //     // console.log(newListContacts)
-        //     return newListContacts;
-        // },
-        // getName:function(){
-        //     const listName=[];
-        //     let listFiltati=this.listaElementiFiltrati();
-        //     for (let index = 0; index < listFiltati.length; index++) {
-        //         listName.push(listFiltati[index].name)
+                return contact.name.toUpperCase().startsWith(this.searchContact.toUpperCase()) || this.searchContact ===''
+            });
+            // console.log(newListContacts)
+            return newListContacts;
+        },
+        getName:function(){
+            const listName=[];
+            let listFiltati=this.listaElementiFiltrati();
+            for (let index = 0; index < listFiltati.length; index++) {
+                listName.push(listFiltati[index].name)
                 
-        //     }
-        //     return listName
-        // },
+            }
+            return listName
+        },
 
-        // isNameIn:function(elem){
-        //     let listaNomi=this.getName();
-        //     if (listaNomi.includes(elem.name)) {
-        //         return true
-        //     }
-        //     return false
-        // }
+        isNameIn:function(elem){
+            let listaNomi=this.getName();
+            if (listaNomi.includes(elem.name)) {
+                return true
+            }
+            return false
+        }
+        */
     },
     // mounted: function(){
-    //     this.autoCiao();
-    //     this.elementiFiltrati();
-    //     this.newListContacts;
+    //     this.lastAccess()
     // },
+    
 
 });
